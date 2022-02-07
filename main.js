33e5c52288bb7a80a66dde5ae8fa221f22fdeac3
premangadiki <87991780+premangadiki@users.noreply.github.com>
2022-02-07 19:49:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video ="";
 status ="";
+object= [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -11,8 +12,33 @@ function setup(){
     canvas.center();
 }
 
+function gotresult(error , results){
+    if(error){
+        console.error(error);
+    }
+
+    else{
+        console.log(results);
+        object = results;
+    }
+}
+
  function draw(){
      image(video , 0 , 0 , 480 , 380);
+     if(status != ""){
+         objectDetector.detect(video , gotresult);
+         for(i=0 ; i < object.length ; i ++){
+             document.getElementById("status").innerHTML = "Status : object detecting";
+             document.getElementById("number_of_objects") .innerHTML = "Number of objects detected :" + object.length;
+
+             percent = floor(object[i].confidence * 100);
+             fill("#FF0000");
+             text(object[i].label + " " + percent + "%" , object[i].x + 15 , object[i].y + 15);
+             noFill();
+             stroke("#FF0000");
+             rect(object[i].x , object[i].y , object[i].width , object[i].height);
+         }
+     }
  }
 
  function start(){
@@ -27,3 +53,5 @@ function setup(){
      video.speed(1);
      video.volume(0);
  }
+
+ 
